5b67570fffe025382932f437d56a253936deb573
wecobol <wecobol@gmail.com>
2018-10-06 23:41:13 +0300

    Summary: - implementation via cycles has been refactored;

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -21,7 +21,7 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        getFilteredWithExceededViaLoops(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
     }
 
     /**
@@ -56,19 +56,9 @@ public class UserMealsUtil {
         final List<UserMealWithExceed> filteredUmList = mealList.stream()
                 .filter(mealFilter)
                 .map(meal -> new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                caloriesPerDateMap.get(meal.getDateTime().toLocalDate()) > caloriesPerDay))
+                        caloriesPerDateMap.get(meal.getDateTime().toLocalDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
 
-//        // Second variant (Optional 2) // but I think this variant is not so clear as the first and requires more
-//        // efforts for further support
-//        mealList.stream().collect(
-//                Collectors.groupingBy(meal ->
-//                        meal.getDateTime().toLocalDate())).forEach((date, uml) -> {
-//            uml.stream().caloriesPerDateMap(meal -> new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-//                    uml.stream().collect(Collectors.summingInt(UserMeal::getCalories)) > caloriesPerDay)).forEach(filteredUmlList::add);
-//        });
-
- //       filteredUmList.forEach(System.out::println);
         return filteredUmList;
     }
 
@@ -83,37 +73,26 @@ public class UserMealsUtil {
      */
     public static List<UserMealWithExceed> getFilteredWithExceededViaLoops(List<UserMeal> mealList, LocalTime startTime,
                                                                            LocalTime endTime, int caloriesPerDay) {
-        final Map<LocalDate, List<UserMeal>> umlPerDateMap = new HashMap<>();
 
-        // grouping UserMeal by date
+        final Map<LocalDate, Integer> calPerDateMap = new HashMap<>();
+
+        // grouping calories by date
         for (UserMeal meal : mealList) {
-            final List<UserMeal> umByDateList = umlPerDateMap.getOrDefault(meal.getDateTime().toLocalDate(), new ArrayList<>());
-            umByDateList.add(meal);
-            umlPerDateMap.put(meal.getDateTime().toLocalDate(), umByDateList);
+            calPerDateMap.merge(meal.getDateTime().toLocalDate(), meal.getCalories(), Integer::sum);
         }
 
         // create an extended list
         final List<UserMealWithExceed> filteredUmList = new ArrayList<>();
 
-        for (List<UserMeal> uml : umlPerDateMap.values()) {
-            int iTotalCalories = 0;
 
-            // summing of calories
-            for (UserMeal meal : uml) {
-                iTotalCalories += meal.getCalories();
-            }
-
-            // filling with @UserMealWithExceed
-            for (UserMeal meal : uml) {
-                final LocalTime mealTime = meal.getDateTime().toLocalTime();
-                if (mealTime.isAfter(startTime) && mealTime.isBefore(endTime)) {
-                    filteredUmList.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                            iTotalCalories > caloriesPerDay));
-                }
+        // filling with @UserMealWithExceed
+        for (UserMeal meal : mealList) {
+            final LocalTime mealTime = meal.getDateTime().toLocalTime();
+            if (mealTime.isAfter(startTime) && mealTime.isBefore(endTime)) {
+                filteredUmList.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
+                        calPerDateMap.get(meal.getDateTime().toLocalDate()) > caloriesPerDay));
             }
         }
-
-//        filteredUmList.forEach(System.out::println);
 
         return filteredUmList;
     }
